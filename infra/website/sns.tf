7c055605d426d4d203ff8830385f9eff3ca82aff
Michael Jones <michael.s.jones@hotmail.co.uk>
2020-10-21 15:27:55 +0100

    sns region change for cdn alerts

--- infra/website/sns.tf
+++ infra/website/sns.tf
@@ -1,8 +1,10 @@
 resource "aws_sns_topic" "aws_website_alarms" {
-    name = "${var.short_name}-alarms"
+    provider    = aws.us-east-1
+    name        = "${var.short_name}-alarms"
 }
 
 resource "aws_sns_topic_subscription" "aws_website_alarm_alerts"{
+    provider    = aws.us-east-1
     count       = length(var.alert_sms)
     topic_arn   = aws_sns_topic.aws_website_alarms.arn
     protocol    = "sms"
